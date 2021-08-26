760fcb1b03b61b4fecaefebd3e152571c4ad965d
nguyendokhachieu <nguyendokhachieu@gmail.com>
2021-08-26 17:11:57 +0700

    add geocoding

--- helpers/index.js
+++ helpers/index.js
@@ -8,11 +8,26 @@ cloudinary.config({
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
+
 module.exports = {
     cloudinary,
     sgMail,
     hideEmail(email) {
         const arr = email.split('@');
         return `${arr[0].substr(0, Math.round(arr[0].length/2))}*****************@${arr[1]}`;
+    },
+    async geocoding(location) {
+        try {
+            const response = await geocodingClient.forwardGeocode({
+                query: location,
+                limit: 1
+            }).send();
+    
+            return response.body.features[0];
+        } catch (error) {
+            return {};
+        }
     }
 }
